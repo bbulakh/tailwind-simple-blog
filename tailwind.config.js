edaf95faa715d0dedbf760287d2ebf56bf5118db
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2023-02-21 17:48:00 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./components/**/*.{html,js}', './**/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/aspect-ratio')],
+}
